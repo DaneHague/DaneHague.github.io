a7ed84390668c531e842c141b25f7a1138f96f12
Dane <danehague1@googlemail.com>
2022-06-24 08:59:42 +0100

    Updates

---
+++ precache-manifest.c82734eb99678f0cbb06b61716633534.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b66369621aef09b424b8",
+    "url": "/static/css/main.bebc532a.chunk.css"
+  },
+  {
+    "revision": "b66369621aef09b424b8",
+    "url": "/static/js/main.b6636962.chunk.js"
+  },
+  {
+    "revision": "9d6d0ad7f97bf131636f",
+    "url": "/static/js/1.9d6d0ad7.chunk.js"
+  },
+  {
+    "revision": "0a619bd8d606a7bec8b2",
+    "url": "/static/js/2.0a619bd8.chunk.js"
+  },
+  {
+    "revision": "5ebfd4f2ddfb36e0fc17",
+    "url": "/static/js/runtime~main.5ebfd4f2.js"
+  },
+  {
+    "revision": "920f4ab15286a34ca84aefed21653fe6",
+    "url": "/static/media/cn1.920f4ab1.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1553c4da9868ceb9655ac614b5b3f5a5",
+    "url": "/index.html"
+  }
+];
